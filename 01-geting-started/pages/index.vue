413f380172af432ef68918f477fa492946332931
mrostami0 <mohammad.rostami13@gmail.com>
2021-09-02 19:47:00 +0430

    01. Getting Started

---
+++ 01-geting-started/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <!--  <Tutorial/>-->
+  <div>
+    <h1>Hi Nuxt JS</h1>
+    <P>I am Mohammad!</P>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
---
+++ 01-geting-started/pages/user.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h3>I Love Programming!</h3>
+      <p>Only DO it!!</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "user"
+}
+</script>
+
+<style scoped>
+
+</style>
